5a39d0e2a401b14b44642b81a15321c0999f01e3
Jesús <jesus.mb.1995@gmail.com>
2025-03-30 21:16:12 +0200

    rust version working

--- rust/mod.rs
+++ rust/mod.rs
@@ -5,33 +5,27 @@ use nix::sys::signal::{self, SigSet, SigmaskHow, Signal};
 use nix::sys::signalfd;
 use std::fs::{File, OpenOptions};
 use std::io::Write;
-use std::os::unix::io::AsRawFd;
 use std::process::Command;
-use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
 struct Daemon {
-    pid_file: String,
-    log_file: String,
     logger: File,
-    signal_fd: Option<signalfd::SignalFd>,
+    signal_fd: signalfd::SignalFd,
 }
 
 impl Daemon {
     pub fn new(pid_file: &str, log_file: &str) -> Result<Self> {
-        let logger = Daemon::start_daemon(pid_file, log_file);
-        let signal_fd = Daemon::setup_signal_handler();
+        let logger = Daemon::start_daemon(pid_file, log_file)?;
+        let signal_fd = Daemon::setup_signal_handler()?;
         Ok(Self {
-            pid_file: pid_file.to_string(),
-            log_file: log_file.to_string(),
             logger,
-            signal_fd ,
+            signal_fd,
         })
     }
 
-    pub fn run() -> Result<()> {
-        daemon.start_tmux_session("Initial")?;
-        daemon.main_loop()
+    pub fn run(&mut self) -> Result<()> {
+        self.start_tmux_session("Initial")?;
+        self.main_loop()
     }
 
     fn start_daemon(pid_file: &str, log_file: &str) -> Result<File> {
@@ -56,19 +50,22 @@ impl Daemon {
     }
 
     fn setup_signal_handler() -> Result<signalfd::SignalFd> {
-        let mut mask = SigSet::empty();
-        mask.add(Signal::SIGUSR1);
+        let mask = {
+            let mut build_mask = SigSet::empty();
+            build_mask.add(Signal::SIGUSR1);
+            build_mask
+        };
 
         signal::sigprocmask(SigmaskHow::SIG_BLOCK, Some(&mask), None)?;
 
         let signal_fd = signalfd::SignalFd::with_flags(&mask, signalfd::SfdFlags::SFD_CLOEXEC)
             .map_err(|e| anyhow!("Failed to create signal fd: {}", e))?;
-        Ok(singal_fd)
+        Ok(signal_fd)
     }
 
     fn main_loop(&mut self) -> Result<()> {
         loop {
-            match self.signal_fd.as_mut().unwrap().read_signal() {
+            match self.signal_fd.read_signal() {
                 Ok(Some(info)) => {
                     if info.ssi_signo == Signal::SIGUSR1 as u32 {
                         let sess_id = format!("SIGUSR1 {}", Local::now());
@@ -117,12 +114,13 @@ impl Daemon {
     }
 
     fn log_message(&self, message: &str) -> Result<()> {
-        writeln!(self.file, "{}", message)?;
+        writeln!(&self.logger, "{}", message)?;
         Ok(())
     }
 }
 
 fn main() -> Result<(), anyhow::Error> {
-    let daemon = Daemon::new("/tmp/tmux_warm_daemon.pid", "/tmp/tmux_warm_daemon.log")?;
+    let mut daemon = Daemon::new("/tmp/tmux_warm_daemon.pid", "/tmp/tmux_warm_daemon.log")?;
     daemon.run()?;
+    Ok(())
 }
